8384680e1e21369ad26f3cf328c27ac8b2f03dec
msczk <maxime43200@gmail.com>
2024-05-18 15:19:35 +0200

    Update layout.blade.php

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="h-100" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head>
-    <body>
+    <body class="d-flex flex-column h-100">
         @include('layout.includes.header')
 
         @yield('content')
