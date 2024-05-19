4065974cbf5753574618bc34ae4889ab37d6c968
msczk <maxime43200@gmail.com>
2024-05-19 15:44:52 +0200

    Show last 6 users on homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function show()
     {
-        return view('home.home');
+        $last_users = User::orderBy('created_at', 'DESC')->take(6)->get();
+        return view('home.home')->with(['last_users' => $last_users]);
     }
     
 }
--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -2,8 +2,21 @@
 
 @section('content')
     <div class="container">
-        <div class="col">
-            <p>coucou</p>
+        <div class="row">
+            <div class="col">
+                <h1>Last users</h1>
+            </div>
+        </div>
+        <div class="row">
+            @foreach ($last_users as $last_user)
+                <div class="col-2">
+                    <div>
+                        <img class="img-fluid " src="https://placehold.co/300x300" alt="{{ $last_user->username }}">
+                    </div>
+                    <div class="text-center ">{{ $last_user->username }}</div>
+                    
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
