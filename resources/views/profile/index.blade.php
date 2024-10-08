d0407a386146ece7df62527e6e7f1e685b54abe3
msczk <maxime43200@gmail.com>
2024-10-08 21:33:22 +0200

    Update index.blade.php

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -52,7 +52,7 @@
         </a>
         <a href="{{ route('profile.bikes') }}" class="col d-flex text-black text-decoration-none justify-content-between py-2">
             <div>
-                <i class="fa-solid fa-route"></i> {{ __('Garage') }}
+                <i class="fa-solid fa-biking"></i> {{ __('Garage') }}
             </div>
             <div class="">
                 <i class="fa fa-chevron-right"></i>
