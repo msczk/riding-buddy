5ce1987a60dc60980da0c2257695ee4fd4196bd6
msczk <maxime43200@gmail.com>
2024-10-18 21:16:13 +0200

    Responsive landing

--- resources/views/home/landing.blade.php
+++ resources/views/home/landing.blade.php
@@ -8,15 +8,15 @@
     </head>
     <body id="landing" class="d-flex flex-column justify-content-end">
         <div class="container">
-            <div class="col-12">
+            <div class="col-12 offset-lg-3 col-lg-6">
                 <h1>
                     {{ __(':name, the app where you make friends and find roads around your location', ['name' => Config::get('app.name')]) }}
                 </h1>
             </div>
-            <div class="col-12">
+            <div class="col-12 offset-lg-3 col-lg-6">
                 <a class="btn btn-primary btn-register w-100" href="{{ route('auth.register') }}">{{ __('Get started') }}</a>
             </div>
-            <div class="col-12 text-center">
+            <div class="col-12 offset-lg-3 col-lg-6 text-center">
                 <a class="btn-guest" href="{{ route('home') }}">{{ __('Continue as guest') }}</a>
             </div>
         </div>
