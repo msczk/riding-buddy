6dd31c5548021f8e061e394aed3e32609af37980
msczk <maxime43200@gmail.com>
2024-10-19 09:00:19 +0200

    Registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,41 +1,57 @@
 @extends('layout.layout')
 
+@section('body_id', 'registration')
+
 @section('content')
 <div class="container">
+    <div class="row">
+        <div class="col">
+            <h1 class="h5">{{ __('Let\'s get started') }}</h1>
+            <p class="small mid-neutral-n80">{{ __('Before we start, we need a few information.') }}</p>
+        </div>
+    </div>
     <div class="row">
         <div class="col col-lg-4 offset-lg-4">
             <form class="my-2" method="POST" action="{{ route('auth.register') }}">
                 @csrf
-                <div class="mb-3">
-                    <label for="username" class="form-label">{{ __('Username') }}</label>
-                    <input required name="username" value="{{ old('username') }}" type="text" class="form-control @error('username') is-invalid @enderror" id="username">
+                <div class="form-floating mb-3">
+                    <input placeholder="{{ __('Username') }}" required name="username" value="{{ old('username') }}" type="text" class="form-control @error('username') is-invalid @enderror" id="username">
+                    
+                    <label for="username">{{ __('Username') }}</label>
+                    
                     @error('username')
                         <div class="invalid-feedback">
                             {{ $message }}
                         </div>
                     @enderror
                 </div>
-                <div class="mb-3">
-                    <label for="email" class="form-label">{{ __('Email address') }}</label>
-                    <input required name="email" value="{{ old('email') }}" type="email" class="form-control @error('email') is-invalid @enderror" id="email">
+                <div class="form-floating mb-3">
+                    <input placeholder="{{ __('Email address') }}" required name="email" value="{{ old('email') }}" type="email" class="form-control @error('email') is-invalid @enderror" id="email">
+                    
+                    <label for="email">{{ __('Email address') }}</label>
+                    
                     @error('email')
                         <div class="invalid-feedback">
                             {{ $message }}
                         </div>
                     @enderror
                 </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">{{ __('Password') }}</label>
-                    <input required name="password" type="password" class="form-control @error('password') is-invalid @enderror" id="password">
+                <div class="form-floating mb-3">
+                    <input placeholder="{{ __('Password') }}" required name="password" type="password" class="form-control @error('password') is-invalid @enderror" id="password">
+                    
+                    <label for="password">{{ __('Password') }}</label>
+                    
                     @error('password')
                         <div class="invalid-feedback">
                             {{ $message }}
                         </div>
                     @enderror
                 </div>
-                <div class="mb-3">
-                    <label for="password_confirmation" class="form-label">{{ __('Password confirmation') }}</label>
-                    <input required name="password_confirmation" type="password" class="form-control @error('password_confirmation') is-invalid @enderror" id="password_confirmation">
+                <div class="form-floating mb-3">
+                    <input required placeholder="{{ __('Password confirmation') }}" name="password_confirmation" type="password" class="form-control @error('password_confirmation') is-invalid @enderror" id="password_confirmation">
+                    
+                    <label for="password_confirmation">{{ __('Password confirmation') }}</label>
+                    
                     @error('password_confirmation')
                         <div class="invalid-feedback">
                             {{ $message }}
@@ -63,7 +79,7 @@
     </div>
     <div class="row mt-4">
         <div class="col text-center">
-            <span class="text-muted small">{{ __('Already have an acccount ?') }}</span> <a class="text-muted small" href="{{ route('auth.login') }}">{{ __('Sign-in') }}</a>
+            <span class="mid-neutral-n80 small">{{ __('Already have an acccount ?') }}</span> <a class="text-muted small" href="{{ route('auth.login') }}">{{ __('Sign-in') }}</a>
         </div>
     </div>
 </div>
